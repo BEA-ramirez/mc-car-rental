79d0717580590d50097cda45d40da744883530c9
Bea Erin Angel L. Ramirez <21-1-01483@vsu.edu.ph>
2026-05-06 20:07:00 +0800

    Forgot to git add again

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React from "react";
-import CheckoutPage from "./checkout/page";
 
 function CustomersPage() {
-  return <CheckoutPage />;
+  return;
+  <div></div>;
 }
 
 export default CustomersPage;
